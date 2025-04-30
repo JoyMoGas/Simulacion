e7bd2ea3f38b292f70b7e57dfe7faac6f54076d4
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2025-04-29 19:33:38 -0700

    First Version

--- .history/home-page/src/App_20250429192611.jsx
+++ .history/home-page/src/App_20250429192611.jsx
@@ -9,12 +9,12 @@ function App() {
         <div className="btn-column">
           <ProjectButton title="8 Reinas" url="https://ocho-reinas-two.vercel.app" subtitle="El problemas de las 8 reinas" />
           <ProjectButton title="Tic-Tac-Toe" url="https://tic-tac-toe-eta-kohl-57.vercel.app" subtitle="Simulacion del juego" />
-          <ProjectButton title="Verificador De Precios" url="https://precios-txt.vercel.app" subtitle="Con archivo TXT" />
+          <ProjectButton title="Verificador De Precios" url="https://proyecto3.vercel.app" subtitle="Con archivo TXT" />
         </div>
         <div className="btn-column">
-          <ProjectButton title="Verificador De Precios" url="https://precios-csv.vercel.app" subtitle="Con archivo CSV" />
-          <ProjectButton title="Verificador De Precios" url="https://precios-json.vercel.app" subtitle="Con archivo JSON" />
-          <ProjectButton title="Verificador De Precios" url="https://precios-sql.vercel.app" subtitle="Con base de datos SQL" />
+          <ProjectButton title="Verificador De Precios" url="https://proyecto4.vercel.app" subtitle="Con archivo CSV" />
+          <ProjectButton title="Verificador De Precios" url="https://proyecto5.vercel.app" subtitle="Con archivo JSON" />
+          <ProjectButton title="Verificador De Precios" url="https://proyecto6.vercel.app" subtitle="Con base de datos SQL" />
         </div>
       </div>
     </>
